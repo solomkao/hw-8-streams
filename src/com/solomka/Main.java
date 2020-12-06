12bb21f0169fe95f65322812236cf8591c845540
Oksana <solomkao@gmail.com>
2020-12-06 15:34:15 +0200

    initial commit

--- src/com/solomka/Main.java
+++ src/com/solomka/Main.java
@@ -13,18 +13,21 @@ public class Main {
         System.out.println("\tBefore processing");
         cars.forEach(System.out::println);
 
-        Map<UUID, Car> mapCars = cars.stream()
+        List<Car> sortedCars = cars.stream()
                 .filter(car -> car.getBrand().equals(Brand.TESLA) || car.getBrand().equals(Brand.AUDI))
                 .filter(car -> car.getYear() >= 2018)
                 .filter(car -> car.getMileage() < 40_000)
                 .sorted((o1, o2) -> Integer.compare(o2.getPrice(), o1.getPrice()))
-                .sorted(Comparator.comparingInt(Car::getPrice))
-                .limit(3)
+                .collect(Collectors.toList());
+        long size = sortedCars.stream().count();
+
+        Map<UUID, Car> mapCars = sortedCars.stream()
+                .skip(size - 3)
                 .collect(Collectors.toMap(car -> car.getId(), car -> car));
 
         System.out.println("\tAfter processing");
         for (Map.Entry<UUID, Car> entry : mapCars.entrySet()) {
-            System.out.println(entry.getValue()+" ["+entry.getKey()+"]");
+            System.out.println(entry.getValue() + " [" + entry.getKey() + "]");
         }
     }
 }
