85f1283e92facb338b7f46a6091a5bee61e5a1ba
Oksana <solomkao@gmail.com>
2020-12-06 19:19:28 +0200

    update

--- src/com/solomka/Main.java
+++ src/com/solomka/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
         List<Car> cars = CarProducer.getListOfCars(200);
-        System.out.println("\tBefore processing");
+        System.out.println("\tBefore processing...");
         cars.forEach(System.out::println);
 
         List<Car> sortedCars = cars.stream()
@@ -21,13 +21,32 @@ public class Main {
                 .collect(Collectors.toList());
         long size = sortedCars.stream().count();
 
+        System.out.println("\n\tAfter sorting...");
+        sortedCars.forEach(System.out::println);
+        System.out.println();
         Map<UUID, Car> mapCars = sortedCars.stream()
                 .skip(size - 3)
                 .collect(Collectors.toMap(car -> car.getId(), car -> car));
 
-        System.out.println("\tAfter processing");
+//        ..............One stream..............
+        Map<UUID, Car> mapCars2 = cars.stream()
+                .filter(car -> car.getBrand().equals(Brand.TESLA) || car.getBrand().equals(Brand.AUDI))
+                .filter(car -> car.getYear() >= 2018)
+                .filter(car -> car.getMileage() < 40_000)
+                .sorted((o1, o2) -> Integer.compare(o2.getPrice(), o1.getPrice()))
+                .sorted(Comparator.comparingInt(Car::getPrice))
+                .limit(3)
+                .collect(Collectors.toMap(car -> car.getId(), car -> car));
+
+        System.out.println("\tAfter processing...");
         for (Map.Entry<UUID, Car> entry : mapCars.entrySet()) {
             System.out.println(entry.getValue() + " [" + entry.getKey() + "]");
         }
+
+        System.out.println("\tAfter processing...");
+        for (Map.Entry<UUID, Car> entry : mapCars2.entrySet()) {
+            System.out.println(entry.getValue() + " [" + entry.getKey() + "]");
+        }
+
     }
 }
